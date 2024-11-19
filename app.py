d489d5aa4f057ee9043764f0bcade84433d00252
Luca D'Angelo <dangelo.luca@iisgalvanimi.edu.it>
2024-11-19 08:45:44 +0000

    aggiunto metodo "aggiungi"

--- app.py
+++ app.py
@@ -12,5 +12,12 @@ def home():
     return render_template('index.html')
 #avvio dell'app Flask
 
+@app.route('/aggiungi', methods=['POST'])
+def aggiungi():
+    elemento = request.form['elemento']
+    if elemento:
+        lista_spesa.append(elemento)
+    return redirect(url_for('home'))
+
 if __name__ == '__main__':
     app.run(debug=True)
