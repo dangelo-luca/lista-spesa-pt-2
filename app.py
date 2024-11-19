c33264deda49921c4ddb6563f4a0b2508ee81cbb
Luca D'Angelo <dangelo.luca@iisgalvanimi.edu.it>
2024-11-19 11:39:12 +0000

    fatto metodo rimuovi prodotto

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ lista_spesa = ["pippo"]
 @app.route('/')
 def home():
 
-    return render_template('index.html')
-#avvio dell'app Flask
+    return render_template('index.html',lista_spesa=lista_spesa)
+
 
 @app.route('/aggiungi', methods=['POST'])
 def aggiungi():
@@ -19,5 +19,12 @@ def aggiungi():
         lista_spesa.append(elemento)
     return redirect(url_for('home'))
 
+@app.route('/rimuovi/<int:indice>', methods=['POST'])
+def rimuovi(indice):
+    if 0 <= indice < len(lista_spesa):
+        lista_spesa.pop(indice)
+    return redirect(url_for('home'))
+
+#avvio dell'app Flask
 if __name__ == '__main__':
     app.run(debug=True)
