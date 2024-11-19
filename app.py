4fd238fd1849bef9fd8798081ebb00e0ddb456d4
Luca D'Angelo <dangelo.luca@iisgalvanimi.edu.it>
2024-11-19 08:36:33 +0000

    creato lista_spesa e fatto condizione su html

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 #inizializza l'app Flask
 
 app = Flask(__name__)
+
+lista_spesa = ["pippo"]
 
 #rotta principale
 @app.route('/')
