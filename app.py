9ffe6fd95aed7c9ba7a89baee51ddb89de8a9a56
Luca D'Angelo <dangelo.luca@iisgalvanimi.edu.it>
2024-11-26 08:42:50 +0000

    creato file models.py

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 #inizializza l'app Flask
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///lista_spesa.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
 
 lista_spesa = ["pippo"]
 
---
+++ models.py
@@ -0,0 +1,5 @@
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
+class ListaSpesa(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    elemento = db.Column(db.String(100), nullable=False)
