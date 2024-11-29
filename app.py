de809c1d1e3520c6592e1431f2ac153c2be59d55
Luca D'Angelo <dangelo.luca@iisgalvanimi.edu.it>
2024-11-29 11:58:12 +0000

    finito lista_spesa pt3

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-#inizializza l'app Flask
+from models import db, ListaSpesa
 
+#inizializza l'app Flask
 app = Flask(__name__)
+
+
+
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///lista_spesa.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -14,29 +18,31 @@ with app.app_context():
 
 lista_spesa = ["pippo"]
 
-#rotta principale
 @app.route('/')
 def home():
-
-    return render_template('index.html',lista_spesa=lista_spesa)
-#
+    lista_spesa = ListaSpesa.query.all()
+    return render_template('index.html',lista=lista_spesa)
 
 @app.route('/aggiungi', methods=['POST'])
 def aggiungi():
     elemento = request.form['elemento']
     if elemento:
-        lista_spesa.append(elemento)
+        nuovo_elemento = ListaSpesa(elemento=elemento) #aggiunge un nuovo elemento alla lista spesa ovvero viene creata una nuova riga nella tabella del Database
+        db.session.add(nuovo_elemento) #in questa riga di codice vengono memorizzati tutti i dati che devono essere aggiunti al Database
+        db.session.commit() #serve per salvare delle modifiche nel Database in questo caso aggiunge l'elemento appena inserito nel Database
     return redirect(url_for('home'))
 
 @app.route('/rimuovi/<int:indice>', methods=['POST'])
 def rimuovi(indice):
-    if 0 <= indice < len(lista_spesa):
-        lista_spesa.pop(indice)
+    elemento = ListaSpesa.query.get(indice) #cerca, tramite l'indice di recuperare una riga dal Database
+    db.session.delete(elemento) #questa riga permette di eliminare un elemento dal Database
+    db.session.commit()
     return redirect(url_for('home'))
 
 @app.route('/svuota', methods = ['POST'])
 def svuota():
-    lista_spesa.clear()
+    ListaSpesa.query.delete() #permette di cancellare tutte le righe del Database
+    db.session.commit()
     return redirect(url_for('home'))
 
 #avvio dell'app Flask
